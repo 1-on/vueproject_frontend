e944b29ac321f57c66cbc3b96198774bf21f6b92
一弦 <2721975880@qq.com>
2023-12-05 12:59:57 +0800

    完成登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <div style="min-height: 100vh; overflow: hidden; display: flex; align-items: center;justify-content: center">
-      <div style="width: 350px">
-        <el-form :model="data.form" label-width="120px">
+    <div class="login-container">
+      <div style="width: 350px" class="login-box">
+        <div style="font-weight:bold;font-size: 24px;text-align: center;margin-bottom: 30px">登 录</div>
+        <el-form :model="data.form">
           <el-form-item>
-            <el-input prefix-icon="User" v-model="data.form.username"/>
+            <el-input prefix-icon="User" v-model="data.form.username" placeholder="请输入账号"/>
+          </el-form-item>
+          <el-form-item>
+            <el-input prefix-icon="Lock" v-model="data.form.password" placeholder="请输入密码"/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" style="width: 100%">登 录</el-button>
           </el-form-item>
         </el-form>
+        <div style="margin-top: 30px;text-align: right">
+          还有没账号？ <a href="/register">注册</a>
+        </div>
       </div>
     </div>
   </div>
@@ -23,5 +33,22 @@ const data = reactive({
 
 
 <style scoped>
+.login-container {
+  min-height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url("@/assets/imgs/bg.png");
+  background-size: cover;
+}
 
+.login-box {
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: 1px;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.7);
+}
 </style>
