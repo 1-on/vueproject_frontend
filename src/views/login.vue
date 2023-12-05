0b0f3fc02e05e2c8fde26f69a513d300a3e762c3
一弦 <2721975880@qq.com>
2023-12-05 12:44:51 +0800

    项目初始化

---
+++ src/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div style="min-height: 100vh; overflow: hidden; display: flex; align-items: center;justify-content: center">
+      <div style="width: 350px">
+        <el-form :model="data.form" label-width="120px">
+          <el-form-item>
+            <el-input prefix-icon="User" v-model="data.form.username"/>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import {reactive} from "vue";
+
+const data = reactive({
+  form: {}
+})
+</script>
+
+
+<style scoped>
+
+</style>
