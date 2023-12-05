40223d0f2fa19035f32a1286c0386c907ed4467d
一弦 <2721975880@qq.com>
2023-12-05 18:59:38 +0800

    开发学生注册功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,29 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Manager',
-      component: () => import('@/views/Manager.vue'),
-      redirect: '/home',
-      children: [
-        {path: 'home', name: 'Home', component: () => import('@/views/manager/Home.vue')},
-        {path: 'course', name: 'Course', component: () => import('@/views/manager/Course.vue')},
-      ]
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('@/views/login.vue'),
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Manager',
+            component: () => import('@/views/Manager.vue'),
+            redirect: '/home',
+            children: [
+                {path: 'home', name: 'Home', component: () => import('@/views/manager/Home.vue')},
+                {path: 'course', name: 'Course', component: () => import('@/views/manager/Course.vue')},
+            ]
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: () => import('@/views/login.vue'),
+        },
+        {
+            path: '/register',
+            name: 'Register',
+            component: () => import('@/views/Regitser.vue'),
+        }
+    ]
 })
 
 export default router
---
+++ src/views/Regitser.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="login-container">
+      <div style="width: 350px" class="login-box">
+        <div style="font-weight:bold;font-size: 24px;text-align: center;margin-bottom: 30px">注 册</div>
+        <el-form :model="data.form" ref="formRef" :rules="rules">
+          <el-form-item prop="username">
+            <el-input prefix-icon="User" v-model="data.form.username" placeholder="请输入账号"/>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input prefix-icon="Lock" show-password v-model="data.form.password" placeholder="请输入密码"/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" style="width: 100%" @click="register">注 册</el-button>
+          </el-form-item>
+        </el-form>
+        <div style="margin-top: 30px;text-align: right">
+          已有账号？请 <a href="/login">登录</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import {reactive, ref} from "vue";
+import {ElMessage} from "element-plus";
+import request from "@/utils/request";
+import router from "@/router";
+
+const data = reactive({
+  form: {}
+})
+const formRef = ref()
+const register = () => {
+  formRef.value.validate((valid) => {
+    if (valid) {
+      request.post('/register', data.form).then(res => {
+        if (res.code === '200') {
+          ElMessage.success("注册成功")
+          router.push('/login')
+        } else {
+          ElMessage.error(res.msg)
+        }
+      })
+    }
+  })
+}
+
+
+const rules = reactive({
+  username: [
+    {required: true, message: '请输入账号', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+  ],
+})
+</script>
+
+
+<style scoped>
+.login-container {
+  min-height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url("@/assets/imgs/bg1.png");
+  background-size: cover;
+}
+
+.login-box {
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: 1px;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.7);
+}
+</style>
